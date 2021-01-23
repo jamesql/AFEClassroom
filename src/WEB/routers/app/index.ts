2aaa1981cedaa9847e826914ded38272a117792f
James <thejamesash1@gmail.com>
2021-01-23 18:26:27 -0500

    add routes for app

--- src/WEB/routers/app/index.ts
+++ src/WEB/routers/app/index.ts
@@ -6,6 +6,21 @@ const app = express.Router();
 app
     .get("/", async (req,res) => {
         res.render("index");
+    })
+    .get("/login", async(req,res) => {
+
+    })
+    .get("/register", async(req,res) => {
+        
+    })
+    .get("/logout", async(req,res) => {
+        
+    })
+    .get("/class", async(req,res) => {
+        
+    })
+    .get("/classroom", async(req,res) => {
+        
     });
 
 export default app;
