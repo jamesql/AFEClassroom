afdf4e820b1a7f59d946ae56eb2e9fec97fa729f
James <thejamesash1@gmail.com>
2021-01-23 17:24:49 -0500

    start fixing routers

--- src/WEB/routers/api/index.ts
+++ src/WEB/routers/api/index.ts
@@ -6,9 +6,6 @@ const app = express.Router();
 app
     .get("/", async (req,res) => {
         res.render("index");
-    })
-    .use("/api", require("./api/index").default)
-    .use("/app", require("./app/index").default)
-    .use("/cdn", require("./cdn/index").default);
+    });
 
 export default app;
