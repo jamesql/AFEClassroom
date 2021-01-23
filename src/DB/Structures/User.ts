ea786da8784c0c68be6a340e15377513fc2f5353
James <thejamesash1@gmail.com>
2021-01-23 12:06:51 -0500

    finish user db structure

--- src/DB/Structures/User.ts
+++ src/DB/Structures/User.ts
@@ -5,13 +5,19 @@ export default class User {
     password: string;
     permission: string;
     classes: string;
+    avi: string;
 
     constructor(data: any) {
         this.load(data);
     }
 
     load(data: any) {
-
+        this.id = data.id;
+        this.username = data.username;
+        this.password = data.password;
+        this.permission = data.permission;
+        this.classes = data.classes;
+        this.avi = data.avi;
     }
 
 }
