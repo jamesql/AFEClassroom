1999864c21ed86d100ffc545f224bde1dbf61c13
James <thejamesash1@gmail.com>
2021-01-22 23:56:37 -0500

    add websocket types

---
+++ src/util/@types/wss.d.ts
@@ -0,0 +1,24 @@
+import WebSocket from "ws";
+
+declare global {
+        namespace AFECWS {
+            interface AFECServer extends WebSocket.Server {
+                clients: Set<ClientSocket>;
+            }
+
+            interface ClientSocket extends WebSocket {
+                type: "client";
+                id: string;
+                authenticated: boolean;
+
+                props: {
+                    userId: string | null;
+                    sequence: number;
+                    lastHeartbeat: number;
+                };
+
+                sendAsync(data: any): Promise<void>;
+
+            }
+        }
+}
