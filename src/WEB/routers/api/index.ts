6be953ed81b5f64a2b70b44aab35bc2788801257
James <thejamesash1@gmail.com>
2021-01-23 17:46:52 -0500

    add login api

--- src/WEB/routers/api/index.ts
+++ src/WEB/routers/api/index.ts
@@ -1,11 +1,22 @@
 /// <refrence path="../../util/@types/global.d.ts" />
 import express from "express";
+import { Authenticate } from "../../../util/Auth"
 
 const app = express.Router();
+const a = new Authenticate();
 
 app
-    .get("/", async (req,res) => {
-        res.render("index");
+    .post("/login", async (req,res) => {
+        const d = req.body;
+        if (!d || !d.username || !d.password) return res.send({code: 400});
+
+        const r = await a.authLogin(d.username, d.password);
+
+        if (r.authed) {
+            
+        } else {
+            
+        }
     });
 
 export default app;
