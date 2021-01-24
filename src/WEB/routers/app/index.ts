cc97acab16f4c744231c85f0d2a45ce94fdf44c7
James <thejamesash1@gmail.com>
2021-01-23 19:49:26 -0500

    set auth check to generic function

--- src/WEB/routers/app/index.ts
+++ src/WEB/routers/app/index.ts
@@ -5,7 +5,7 @@ const app = express.Router();
 
 app
     .get("/", async (req,res) => {
-        if (!req.user) return res.redirect("/app/login");
+        await checkAuth(req,res);
         res.render("index");
     })
     .get("/login", async(req,res) => {
@@ -23,5 +23,7 @@ app
     .get("/classroom", async(req,res) => {
         
     });
+
+    function checkAuth(req: express.Request, res: express.Response) { if(!req.user) return res.redirect("/app/login"); }
 
 export default app;
