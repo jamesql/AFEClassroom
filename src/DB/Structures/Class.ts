33139b4ad53bcd35d70caacc89a84445093bb26e
James <thejamesash1@gmail.com>
2021-01-25 15:21:32 -0500

    finish class structure

--- src/DB/Structures/Class.ts
+++ src/DB/Structures/Class.ts
@@ -1,16 +1,25 @@
+import { ExecFileOptionsWithStringEncoding } from "child_process";
+
 export { Class };
 export default class Class {
+    id: string;
+    teacher: string;
+    description: string;
+    name: string;
+    t_name: string;
+    categorys: string;
 
     constructor(data: any) {
         this.load(data);
     }
 
     load(data: any) {
-
-    }
-
-    getById(id: string) {
-        
+        this.id = data.id;
+        this.teacher = data.id;
+        this.description = data.description;
+        this.name = data.name;
+        this.t_name = data.t_name;
+        this.categorys = data.categorys;
     }
 
 }
