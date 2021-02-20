960ae61531f960bd8858aeb7faa212531b085d10
James <thejamesash1@gmail.com>
2021-02-19 20:07:51 -0500

    start working on upload.js

--- src/WEB/public/scripts/upload.js
+++ src/WEB/public/scripts/upload.js
@@ -1,1 +1,14 @@
 // Upload.js - AFEClassroom
+
+async function upload(file, directory, id)
+{
+    const req = await fetch(`http://${window.location.hostname}/${directory}/${id}`, 
+        {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                "file": file
+            }
+        }
+    ); 
+}
