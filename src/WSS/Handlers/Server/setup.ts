29311ea5837817f3ce5b3f7fc49e23a5ce7dcb5a
James <thejamesash1@gmail.com>
2021-02-19 21:32:01 -0500

    add values type file

--- src/WSS/Handlers/Server/setup.ts
+++ src/WSS/Handlers/Server/setup.ts
@@ -1,10 +1,15 @@
+/// <reference path="../../../util/@types/wss.d.ts" />
+
 import * as http from "http";
 import * as https from "https";
 import WebSocket from "ws";
 import cfg from "../../../config"
 
 // add https
-export default async function StartWSS() : Promise<http.Server> {
-    const ws = new WebSocket.Server
-    return null;
+export default async function StartWSS(server: http.Server) : Promise<AFECWS.AFECServer> {
+    const ws = new WebSocket.Server({ server }) as AFECWS.AFECServer;
+
+    
+
+    return ws;
 };
