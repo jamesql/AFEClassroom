354959090112cc54bcd220c04439e1df7220e607
James <thejamesash1@gmail.com>
2021-03-05 19:46:20 -0500

    continue wss

--- src/WSS/Handlers/Server/Connection.ts
+++ src/WSS/Handlers/Server/Connection.ts
@@ -12,6 +12,17 @@ export default (async (ws:AFECWS.AFECServer, skt: AFECWS.ClientSocket, rq: Incom
         lastHeartbeat: Date.now()
     }    
 
-    
+    skt
+        .on("message", require("./Message").bind(null,ws,skt,rq))
+        .on("close", require("./Close").bind(null,ws,skt,rq))
+        .on("error", require("./error").bind(null,ws,skt,rq));
+
+
+    skt.authenticated = false;
+
+    // send hello packet
+    skt.sendAsync({
+        
+    });
 
 });
