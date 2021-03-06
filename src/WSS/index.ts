fa287d011e4729bbdfe7cb5231e755e2280e1697
James <thejamesash1@gmail.com>
2021-03-05 20:01:37 -0500

    add ws options to config

--- src/WSS/index.ts
+++ src/WSS/index.ts
@@ -1,6 +1,11 @@
 /// <refrence path="../util/@types/global.d.ts" />
 import StartWSS from "./Handlers/Server/setup"
+import * as http from "http";
+
+
+const sv = http.createServer({});
+
 
 export default async function launchWSS() {
-    //await StartWSS();
+    await StartWSS();
 };
--- src/config/index.ts
+++ src/config/index.ts
@@ -27,6 +27,10 @@ const config = {
 			user: "afe",
 			password: "password"
 		}
+	},
+	ws: {
+		host: "127.0.0.1",
+		port: 1337
 	}
 
 };
