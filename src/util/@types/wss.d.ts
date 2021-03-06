f7c632c627900c653a4b92f3da2b20d4fc39f004
James <thejamesash1@gmail.com>
2021-03-05 19:50:37 -0500

    start op codes and packets

--- src/util/@types/wss.d.ts
+++ src/util/@types/wss.d.ts
@@ -21,4 +21,8 @@ declare global {
 
             }
         }
+
+        namespace Packets {
+            
+        }
 }
---
+++ src/util/WSValues.ts
@@ -0,0 +1,6 @@
+export const OPCodes = {
+    HELLO: 0,
+    HEARTBEAT: 1,
+    HEARTBEAT_ACK: 2,
+    ERROR: 3,
+} as const;
