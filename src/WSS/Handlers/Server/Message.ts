121da745828e11a5dc8ab37b739b109620b00e15
James <thejamesash1@gmail.com>
2021-03-05 20:15:45 -0500

    start message handler

--- src/WSS/Handlers/Server/Message.ts
+++ src/WSS/Handlers/Server/Message.ts
@@ -1,1 +1,24 @@
-export default null;
+import { createCipher } from "crypto";
+import { IncomingMessage } from "http";
+import WebSocket from "ws";
+
+export default (async (ws:AFECWS.AFECServer, skt: AFECWS.ClientSocket, rq: IncomingMessage, payload: WebSocket.Data) => {
+    let dt;
+
+    try {
+        dt = JSON.parse(payload.toString());
+    } catch(e) {
+        dt = null;
+    }
+
+    // close with error
+    if (dt === null) return skt.close();
+
+    // check if authed.
+
+    // Message Handler
+    switch (dt.op) {
+
+    }
+
+});
