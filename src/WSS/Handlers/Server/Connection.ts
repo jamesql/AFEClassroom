3e45882ebab88bf101205680c67a7f6f8ab27f44
James <thejamesash1@gmail.com>
2021-03-05 23:35:16 -0500

    add debug messages to console

--- src/WSS/Handlers/Server/Connection.ts
+++ src/WSS/Handlers/Server/Connection.ts
@@ -1,10 +1,10 @@
 import { IncomingMessage } from "http";
 import { HEARTBEAT_INTERVAL, OPCodes } from "../../../util/WSValues";
-
+import {colors as leeks} from "leeks.js";
 
 export default (async (ws:AFECWS.AFECServer, skt: AFECWS.ClientSocket, rq: IncomingMessage) => {
 
-    console.log(`[AFEClassroom - WS] [Client>>Server] Incoming Connection from ${rq.connection.remoteAddress} (${skt.id})`);
+    console.log(leeks.magenta(`[AFEClassroom - WS] [Client>>Server] Incoming Connection from ${rq.connection.remoteAddress} (${skt.id})`));
 
     // init props
     skt.props = {
--- src/util/WSFunctions.ts
+++ src/util/WSFunctions.ts
@@ -1,8 +1,10 @@
 import WebSocket from "ws";
+import {colors as leeks} from "leeks.js";
 
 Object.defineProperty(WebSocket.prototype, "sendAsync", {
     async value(this: AFECWS.ClientSocket, d: any) {
 		if (this.readyState !== WebSocket.OPEN) return;
+        console.log(leeks.green(`[AFEClassroom - WS] [Server>>Client] Sent OP Code >${d.op}< to ${this.id}`));
 		return new Promise<void>((a, b) =>
 			this.send(typeof d !== "string" ? JSON.stringify({
 				...d,
